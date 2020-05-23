9137a98d47a813295d364c4173a6113f613093fc
liusCoding <14786469221@163.com>
2020-05-23 11:31:49 +0800

    测试spring 声明式事务成功

--- spring-annotation/src/main/java/com/liuscoding/tx/UserService.java
+++ spring-annotation/src/main/java/com/liuscoding/tx/UserService.java
@@ -2,6 +2,7 @@ package com.liuscoding.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @className: UserService
@@ -16,7 +17,7 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
-
+    @Transactional(rollbackFor = Exception.class)
     public void insertUser(){
         userDao.insert();
         System.out.println("----------插入完成-------");
