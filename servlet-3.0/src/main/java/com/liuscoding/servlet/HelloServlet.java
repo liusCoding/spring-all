7718aed2b855c6d33f0c78d78b0a0c37be6dffc9
liusCoding <14786469221@163.com>
2020-05-24 14:03:05 +0800

    servlet3.0异步处理

--- servlet-3.0/src/main/java/com/liuscoding/servlet/HelloServlet.java
+++ servlet-3.0/src/main/java/com/liuscoding/servlet/HelloServlet.java
@@ -15,10 +15,22 @@ import java.io.IOException;
  */
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //super.doGet(req, resp);
+        System.out.println(Thread.currentThread()+" start...");
+        try {
+            sayHello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resp.getWriter().write("hello...");
+        System.out.println(Thread.currentThread()+" end...");
+    }
 
-        resp.getWriter().write("hello.......");
+    public void sayHello() throws Exception{
+        System.out.println(Thread.currentThread()+" processing...");
+        Thread.sleep(3000);
     }
 }
